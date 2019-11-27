c60187dea89a3bbbdd681af22694bea1db72b531
Flounder <testy@test.com>
2019-11-26 22:24:30 -0600

    fifty states

---
+++ client/src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="about">
+		<h2>Where have you traveled to?</h2>
+		<p>You can use this site to track what states you have visitee</p>
+		<p>A Vue.js site by Carson Richmond.</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'About'
+}
+</script>
---
+++ client/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="footer">
+		<div class="m-2">
+			<span class="p-3">
+				<router-link to="/">Home</router-link>
+			</span>
+			<span class="p-3">
+				<router-link to="/about">About</router-link>
+			</span>
+		</div>
+
+		<p class="text-right p-2">Header image by:..</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Footer'
+}
+</script>
+
+<style scoped></style>
